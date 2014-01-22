1287f0b387d85b44e155317c4572f408f512e2d6
Kelsey Young <kelseyyoung4@gmail.com>
2014-01-22 15:55:17 -0700

    Initial Commit

---
+++ InterviewTracker/InterviewTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InterviewTracker.Startup))]
+namespace InterviewTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
